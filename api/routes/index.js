c5a1b955b983c8c46ca851cf9aa15cf18b5479d8
EleazarSF <eleazarsf@gmail.com>
2022-12-10 09:59:32 -0300

    cors added to the routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const birthdays = require('./birthdaysRoute');
 
 module.exports = app => {
-    app.use(bodyParser.urlencoded( { extended: true } ));
+    app.use(bodyParser.json());
+    app.use(cors());
     app.use(birthdays);
 };
